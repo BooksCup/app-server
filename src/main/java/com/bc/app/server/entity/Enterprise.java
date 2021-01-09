8e596dc1cbbab69e3a9ba454dde8d4695ada24ce
BooksCup <812809680@qq.com>
2021-01-09 13:53:05 +0800

    企业新增字段

--- src/main/java/com/bc/app/server/entity/Enterprise.java
+++ src/main/java/com/bc/app/server/entity/Enterprise.java
@@ -10,6 +10,11 @@ public class Enterprise {
     private String id;
     private String name;
     private String shortName;
+    private String legalPersonName;
+    private String estiblishDate;
+    private String regLocation;
+    private String regStatus;
+    private String creditCode;
 
     public String getId() {
         return id;
@@ -33,6 +38,46 @@ public class Enterprise {
 
     public void setShortName(String shortName) {
         this.shortName = shortName;
+    }
+
+    public String getLegalPersonName() {
+        return legalPersonName;
+    }
+
+    public void setLegalPersonName(String legalPersonName) {
+        this.legalPersonName = legalPersonName;
+    }
+
+    public String getEstiblishDate() {
+        return estiblishDate;
+    }
+
+    public void setEstiblishDate(String estiblishDate) {
+        this.estiblishDate = estiblishDate;
+    }
+
+    public String getRegLocation() {
+        return regLocation;
+    }
+
+    public void setRegLocation(String regLocation) {
+        this.regLocation = regLocation;
+    }
+
+    public String getRegStatus() {
+        return regStatus;
+    }
+
+    public void setRegStatus(String regStatus) {
+        this.regStatus = regStatus;
+    }
+
+    public String getCreditCode() {
+        return creditCode;
+    }
+
+    public void setCreditCode(String creditCode) {
+        this.creditCode = creditCode;
     }
 
 }
